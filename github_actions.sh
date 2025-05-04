4ac45b82f36a713cb87a269031927d05f5376f96
jimrothstein <jimrothstein@gmail.com>
2025-05-03 23:37:23 -0700

    explict install knitr, rmarkdown

--- github_actions.sh
+++ github_actions.sh
@@ -1,4 +1,6 @@
-gh api repos/jimrothstein/gh_actions_project/actions/runs
+# REF:  https://devblog.jpcaparas.com/bulk-delete-github-actions-workflow-runs-with-a-simple-bash-function-b9105009a579
 
-visual
-visual
+# jq is json processor
+gh api \
+ repos/jimrothstein/gh_actions_project/actions/runs --paginate | jq -r '.workflow_runs[] | .id'
+
